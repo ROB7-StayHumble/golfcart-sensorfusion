9443a7f5b7ddfd4dddb7886397d4fc364d4d838a
glhr <grenblot@gmail.com>
2019-10-30 12:46:49 +0100

    plot IR alongside LIDAR

--- pyqtgraph-lidar.py
+++ pyqtgraph-lidar.py
@@ -21,23 +21,46 @@ app = QtGui.QApplication([])            # you MUST do this once (initialize thin
 ####################
 
 win = pg.GraphicsWindow(title="Golfcart sensors") # creates a window
-p_lidar = win.addPlot(title="LIDAR data")  # creates empty space for the plot in the window
-curve_lidar = p.plot()                        # create an empty "plot" (a curve to plot)
+#win_layout = win.addLayout(row=0, col=0)
+#plot_view = win_layout.addViewBox(0,0)
+#img_view = win_layout.addViewBox(0,1)
+
+p_lidar = win.addPlot(title="LIDAR data",labels={'left':'Range (meters)','bottom':'Angle (degrees)'})  # creates empty space for the plot in the window
+#plot_view.addItem(p_lidar)
+curve_lidar = p_lidar.plot()                        # create an empty "plot" (a curve to plot)
+p_lidar.setYRange(0, 50, padding=0)
+p_lidar.showGrid(x=True,y=True)
+
+p_ir = win.addPlot(title = 'IR cam')
+imgItem = pg.ImageItem()
+curve_ir = p_ir.plot()
+curve_ir.getViewBox().invertY(True)
+p_ir.addItem(imgItem)
 
 angles = np.arange(-90,90.5,0.5)
 
 # Realtime data plot. Each time this function is called, the data display is updated
-def update(ranges):
+def update_lidar(lidar_ranges):
     global curve_lidar, angles    
-    curve_lidar.setData(angles,ranges)          # set the curve with this data
+    curve_lidar.setData(angles,lidar_ranges)          # set the curve with this data
+    QtGui.QApplication.processEvents()    # you MUST process the plot now
+
+# Realtime data plot. Each time this function is called, the data display is updated
+def update_ir(image):
+    global imgItem, img_view    
+    imgItem.setImage(image,autoDownsample=True)          # set the curve with this data
     QtGui.QApplication.processEvents()    # you MUST process the plot now
 
 ### MAIN PROGRAM #####    
 # this is a brutal infinite loop calling your realtime data plot
 for topic, msg, t in bag.read_messages():	
-    print t, topic
+    #print t, topic
     if topic == '/bottom_scan':
-	    update(msg.ranges)
+	    update_lidar(msg.ranges)
+    elif topic == '/ircam_data':
+	    image = bridge.imgmsg_to_cv2(msg, "bgr8")
+	    image = np.swapaxes(image,0,1)
+	    update_ir(image)
 
 
 ### END QtApp ####
