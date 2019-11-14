d9139209f51bc83b46891eb714a046dad523a762
glhr <grenblot@gmail.com>
2019-11-14 15:36:33 +0100

    added BRISK descriptor matching

--- sensorfusion/cameramatching/test_feature_detectors.py
+++ sensorfusion/cameramatching/test_feature_detectors.py
@@ -11,24 +11,18 @@ zed = cv2.imread('zedcam'+folder+'.png',0)
 
 # code based on https://docs.opencv.org/3.4/dc/dc3/tutorial_py_matcher.html
 
-# Initiate SIFT detector
+# Initiate ORB detector
 orb = cv2.ORB_create()
-
 kp1, des1 = orb.detectAndCompute(ir,None)
 kp2, des2 = orb.detectAndCompute(zed,None)
-
 # create BFMatcher object
 bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-
 # Match descriptors.
 matches = bf.match(des1,des2)
-
 # Sort them in the order of their distance.
 matches = sorted(matches, key = lambda x:x.distance)
-
 # Draw first 10 matches.
 img3 = cv2.drawMatches(ir,kp1,zed,kp2,matches[:20], None, flags=2)
-
 plt.imshow(img3)
 plt.savefig("feature_matching/feature_matching_orb.png", bbox_inches='tight', pad_inches=0)
 plt.show()
@@ -50,4 +44,20 @@ for m,n in matches:
 img3 = cv2.drawMatchesKnn(ir,kp1,zed,kp2,good,None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
 plt.imshow(img3)
 plt.savefig("feature_matching/feature_matching_sift.png", bbox_inches='tight', pad_inches=0)
+plt.show()
+
+# Initiate BRISK detector
+brisk = cv2.BRISK_create()
+kp1, des1 = brisk.detectAndCompute(ir,None)
+kp2, des2 = brisk.detectAndCompute(zed,None)
+# create BFMatcher object
+bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+# Match descriptors.
+matches = bf.match(des1,des2)
+# Sort them in the order of their distance.
+matches = sorted(matches, key = lambda x:x.distance)
+# Draw first 10 matches.
+img3 = cv2.drawMatches(ir,kp1,zed,kp2,matches[:20], None, flags=2)
+plt.imshow(img3)
+plt.savefig("feature_matching/feature_matching_brisk.png", bbox_inches='tight', pad_inches=0)
 plt.show()
